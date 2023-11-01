778f9440b26f43b90cd2da20d308ea4499099c5b
Bagus Pramudya <bagusesap@gmail.com>
2023-11-01 11:21:22 +0700

    add code to extract json

--- app-json.py
+++ app-json.py
@@ -15,7 +15,39 @@ vectorizer = joblib.load('tfidf_vectorizer.pkl')
 def predict():
     data = json.loads(request.data)
 
-    df = pd.DataFrame(data)
+    rows = []
+    for obj in data:
+        # Extract the relevant data from the object
+        name = obj['participant']['name']
+        title1 = obj['internshipRoles'][0]['title']
+        title2 = obj['internshipRoles'][1]['title'] if len(
+            obj['internshipRoles']) > 1 else None
+        title3 = obj['internshipRoles'][2]['title'] if len(
+            obj['internshipRoles']) > 2 else None
+        educational_backgrounds = obj['participant']['curriculumVitae']['educationalBackgrounds']
+        organizational_experience = obj['participant']['curriculumVitae']['organizationExperiences']
+        work_experiences = obj['participant']['curriculumVitae']['workExperiences']
+        skill = obj['participant']['curriculumVitae']['skill']
+        final_task = obj['participant']['curriculumVitae']['finalTask']
+        achievement = obj['participant']['curriculumVitae']['achievements']
+
+        # Create a new row with the extracted data
+        row = {
+            'Name': name,
+            'Role 1': title1,
+            'Role 2': title2,
+            'Role 3': title3,
+            'Riwayat Pendidikan': educational_backgrounds,
+            'Pengalaman Organisasi': organizational_experience,
+            'Pengalaman Bekerja': work_experiences,
+            'Skill': skill,
+            'Tugas Akhir': final_task,
+            'Prestasi': achievement
+        }
+
+        rows.append(row)
+
+    df = pd.DataFrame(rows)
     df = df.astype(str)
 
     X = df.drop(columns=['role','nama'])
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
-from sklearn.svm import SVC
+
 
 app = Flask(__name__)
 
